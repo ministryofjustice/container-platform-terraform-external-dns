9e85240eff55adb8a480557efcf28cb237514b7c
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2026-07-07 12:53:08 +0100

    fix: relax provider version constraints for kubectl and time

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     kubectl = {
       source  = "alekc/kubectl"
-      version = "2.4.1"
+      version = ">= 2.0.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.14.0"
+      version = ">= 0.13.0"
     }
   }
   required_version = "~> 1.0"
